f1d5458b3c88b6260d50bc73a43f3519910e25bc
Christian Spiller <christian.spiller@chaosnet.ch>
2022-04-28 22:22:37 +0200

    removed custom-grid

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CustomGridComponent } from './custom-grid/custom-grid.component';
 import { StickyGridComponent } from './sticky-grid/sticky-grid.component';
 import { RowGroupComponent } from './sticky-grid/row-group/row-group.component';
 import { HeaderRowGroupComponent } from './sticky-grid/header-row-group/header-row-group.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CustomGridComponent, StickyGridComponent, RowGroupComponent, HeaderRowGroupComponent],
-  exports: [CustomGridComponent, StickyGridComponent]
+  declarations: [StickyGridComponent, RowGroupComponent, HeaderRowGroupComponent],
+  exports: [StickyGridComponent]
 })
 export class SharedModule {}
